59660492c76e340a8d9c33ab394ce24e92b9dc71
RIYASUDEEN27 <riyasudeenit27@gmail.com>
2026-07-07 20:54:11 +0530

    Hardcode settings to bypass Render env vars

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,12 +1,13 @@
+from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    mongodb_url: str
-    database_name: str
-    secret_key: str
-    algorithm: str
-    access_token_expire_minutes: int
+    mongodb_url: str = Field(default="mongodb+srv://riyasudeenit27_db_user:Gz3Y6mADElTneNmv@simpletask.mqxjb4u.mongodb.net/?appName=simpletask")
+    database_name: str = Field(default="smart_library")
+    secret_key: str = Field(default="supersecretkey_please_change_in_production")
+    algorithm: str = Field(default="HS256")
+    access_token_expire_minutes: int = Field(default=1440)
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 settings = Settings()
